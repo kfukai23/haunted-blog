52e712ec13603ddbc3a922204e7e73babfc33c9e
Kaito Fukai <k.fukai@everyleaf.com>
2022-03-26 16:54:58 +0900

    ブログ編集・更新・秘密記事閲覧まわりの権限設定を見直し
    - 他人のブログの編集画面にアクセスできないようにする
    - 他人のブログは更新できないようにする
    - 他人のブログは削除できないようにする
    - 他人の秘密のブログには、ログイン状態にかかわらずアクセスできないようにする
      - 言い換えるとアクセスできるのは「秘密記事でない全ての記事 + （ログインしている場合）自分の秘密記事」という状態にする

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,13 +3,22 @@
 class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
-  before_action :set_blog, only: %i[show edit update destroy]
+  before_action :set_blog, only: %i[edit update destroy]
 
   def index
     @blogs = Blog.search(params[:term]).published.default_order
   end
 
-  def show; end
+  def show
+    visible_blogs =
+      if current_user
+        current_user.blogs.or(Blog.published)
+      else
+        Blog.published
+      end
+
+    @blog = visible_blogs.find(params[:id])
+  end
 
   def new
     @blog = Blog.new
@@ -44,7 +53,7 @@ class BlogsController < ApplicationController
   private
 
   def set_blog
-    @blog = Blog.find(params[:id])
+    @blog = current_user.blogs.find(params[:id])
   end
 
   def blog_params
